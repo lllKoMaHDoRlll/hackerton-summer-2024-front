afcff04b719f70ef0ec312faee46c3fd16ba0d9f
Koma <aleksandr.orlichenya@gmail.com>
2024-08-03 12:00:20 +0300

    implement map component and integrate it to page

---
+++ src/components/PartnersMap.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { IonCardContent, IonCard } from "@ionic/react";
+
+import styled from "styled-components";
+
+const MapFrame = styled.iframe`
+    width: 100%;
+`
+
+export default function PartnerMap() {
+    return (
+        <IonCard>
+            <IonCardContent>
+                <MapFrame src="https://yandex.ru/map-widget/v1/?um=constructor%3A39780d8766c0e704746b601773cdf42fcd031ce0fd7e43967fa61fe598c656df&amp;source=constructor" width="400" height="400"></MapFrame>
+            </IonCardContent>
+        </IonCard>
+    )
+}
--- src/pages/Employer/MapOfPartners.tsx
+++ src/pages/Employer/MapOfPartners.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { IonPage } from "@ionic/react";
+import { IonBackButton, IonPage, IonTitle, IonHeader, IonToolbar, IonButtons, IonContent, IonCardContent, IonCard, IonList, IonItem, IonListHeader, IonLabel } from "@ionic/react";
+
+import styled from "styled-components";
+import PartnerMap from "../../components/PartnersMap";
+
+const MapFrame = styled.iframe`
+    width: 100%;
+`
 
 export default function MapOfPartners() {
     return (
         <IonPage>
-            
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Карта партнеров</IonTitle>
+                </IonToolbar>
+                <IonButtons slot="start">
+                    <IonBackButton></IonBackButton>
+                </IonButtons>
+            </IonHeader>
+            <IonContent>
+                <PartnerMap></PartnerMap>
+                <IonList>
+                    <IonListHeader>
+                        <IonLabel>Партнеры</IonLabel>
+                    </IonListHeader>
+                    <IonItem></IonItem>
+                </IonList>
+            </IonContent>
         </IonPage>
     )
 }
