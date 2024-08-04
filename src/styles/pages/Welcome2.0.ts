a7391820c8affd277f51b6223d4e7130b6a5e878
unknown <sanyapridava@mail.ru>
2024-08-04 08:02:14 +0300

    Стилизованные компоненты для элементов окна с лого

---
+++ src/styles/pages/Welcome2.0.ts
@@ -0,0 +1,16 @@
+import { IonPage, IonText } from "@ionic/react";
+import styled from "styled-components";
+
+export const Page = styled(IonPage)`
+    width: 100%;
+    height: 100%;
+    background-color: #1F3040;
+    color: white;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Text = styled(IonText)`
+    font-size: 65px;
+    font-weight: 700;
+`
