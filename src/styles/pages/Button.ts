37ba1d2a54d47f8267775f12f16b2a97b5748ca8
unknown <sanyapridava@mail.ru>
2024-08-04 10:14:46 +0300

    Дефолтные стили для страниц и кнопок

---
+++ src/styles/pages/Button.ts
@@ -0,0 +1,6 @@
+import { IonButton } from "@ionic/react";
+import styled from "styled-components";
+
+export const Button = styled(IonButton)`
+    --background: #E892D7;
+`
---
+++ src/styles/pages/Page.ts
@@ -0,0 +1,8 @@
+import { IonPage } from "@ionic/react";
+import styled from "styled-components";
+
+export const Page = styled(IonPage)`
+    width: 100%;
+    height: 100%;
+    --ion-background-color: #1F3040;
+`
